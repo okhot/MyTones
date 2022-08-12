ddde67980be747ab9d67b28c9748c04db2a7eb3b
okhot <arreyetta1jnr@gmail.com>
2022-08-12 11:59:10 +0100

    NavBar Design

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <secction>
+        <div className="navbar">
+
+            <div className="navbar__link">
+                <li to='/' className="navbar__link">Music</li>
+                <li to='/' className="navbar__link">Podcast</li>
+                <li to='/' className="navbar__link">Cast</li>
+                <li to='/' className="navbar__link">Live</li>
+            </div>
+        </div>
+    </secction>
+  )
+}
+
+export default NavBar
